94845af36ede98b3a3141abaa3f67b3d6321b032
Asano-H <princessknk0406@gmail.com>
2017-09-21 21:08:41 +0900

    commit

---
+++ list.py
@@ -0,0 +1,18 @@
+datalist=[1,2,3]	#リスト
+datatuple=(1,2,3)	#タプル
+
+print(datalist)
+print(datatuple)
+
+print(datalist[2])	#datalistの３つ目の要素を出力
+print(datatuple[0])	#datatupleの１つ目の要素を出力
+
+
+"""
+data1=1
+data2=2
+data3=3
+をまとめた形。配列。
+リストは定義した後に要素の変更が可能。
+タプルは変更が不可能。
+"""
