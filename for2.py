eed39a5434e3bbce113a24a3bddf6803a0ca8a8e
Asano-H <princessknk0406@gmail.com>
2017-09-21 22:36:37 +0900

    commit

---
+++ for2.py
@@ -0,0 +1,20 @@
+for ten in range(10):
+	print(ten)
+for twelve in range(8,12):
+	print(twelve)
+
+for eight in range(10,50,8):
+	print(eight)
+"""
+range関数
+
+range(10)
+変数tenに０～９までの数字が繰り返し代入される。
+
+range(8,12)
+変数twelveに８～１２までの数字が代入される。
+
+range(10,50,8)
+変数eightに１０～５０までの数字の中で、８ずつ増加させた値が代入される。
+
+"""
