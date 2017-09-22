7d545db59786501b36e997f1767a146dd896f257
Asano-H <princessknk0406@gmail.com>
2017-09-22 15:43:54 +0900

    commit

---
+++ endswith.py
@@ -0,0 +1,13 @@
+data="apple juice"
+
+print(data.endswith("le"))
+print(data.endswith("apple"))
+
+print(data.endswith("ce"))
+print(data.endswith("juice"))
+
+"""
+endswith
+指定した文字列で終わるか調べる
+真であればTrue　偽ならFalseを返す
+"""
