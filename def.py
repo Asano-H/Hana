14c2c76d7dbd6165652b8e6fc39b8bbb5db1e311
Asano-H <princessknk0406@gmail.com>
2017-09-22 15:08:06 +0900

    add

--- def.py
+++ def.py
@@ -1,8 +1,15 @@
-def cal(x,y,z):
-	answer=x+y-z
-	return answer
+def cal(x,y,z):	#引数ｘ、ｙ、ｚを使う
+	answer=x+y-z	#answerを計算
+	return answer	#戻り値としてanswerを返す
 
 print(cal(10,2,4))
 
+"""
+関数
+def 関数名(引数１、引数２):
+	処理１
+	処理２
+	return 戻り値
 
-
+引数・戻り値は、複数の場合も無い場合もある。
+"""
