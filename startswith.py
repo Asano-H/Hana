adcf8a8f9b7181c24ccf04a2a5caa679035daa9e
Asano-H <princessknk0406@gmail.com>
2017-09-22 15:43:33 +0900

    commit

---
+++ startswith.py
@@ -0,0 +1,13 @@
+data="apple juice"
+
+print(data.startswith("ap"))
+print(data.startswith("apple"))
+
+print(data.startswith("ju"))
+print(data.startswith("juice"))
+
+"""
+startswith
+指定した文字列で始まるか調べる
+真であればTrue　偽でならFalseを返す。
+"""
