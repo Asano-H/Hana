a2f3017f493aa60ffb3723a349c3dd0d2714f31c
Asano-H <princessknk0406@gmail.com>
2017-09-22 11:30:41 +0900

    commit

---
+++ split.py
@@ -0,0 +1,16 @@
+str="apple"+"orange"	#文字列＋文字列
+print(str)
+
+a="red"
+b="blue"
+c=a+b	#文字列型の変数＋文字列型の変数
+print(c)
+
+data="Hello,everyone"
+print(data.split(","))	#[,]を区切り文字に指定
+"""
+split関数
+引数に文字列を分割する区切り文字を指定すると、リスト形式で分割した結果がかえってくる。
+文字列の連結は[+]を使う
+
+"""
