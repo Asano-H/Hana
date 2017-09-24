baa6b50060b59b4314f0773957d11139b46e8c80
Asano-H <princessknk0406@gmail.com>
2017-09-24 11:55:50 +0900

    commit

--- list.py
+++ list.py
@@ -1,18 +1,21 @@
-datalist=[1,2,3]	#リスト
-datatuple=(1,2,3)	#タプル
+datalist=[1,2,3]	#リスト型
+datatuple=(1,2,3)	#タプル型
+datajisyo={"apple":1,"orange":2}	#辞書型
 
 print(datalist)
 print(datatuple)
+print(datajisyo)
 
 print(datalist[2])	#datalistの３つ目の要素を出力
 print(datatuple[0])	#datatupleの１つ目の要素を出力
-
+print(datajisyo["apple"])	#1
 
 """
 data1=1
 data2=2
 data3=3
 をまとめた形。配列。
-リストは定義した後に要素の変更が可能。
-タプルは変更が不可能。
+リスト・辞書型は定義した後に要素の変更が可能。
+タプル型は変更が不可能。
+辞書型は要素ではなく(文字列や数値)を使って要素を指定します。
 """
