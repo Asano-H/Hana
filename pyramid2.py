e8418091ee9eeb62548a21d753ecd59d9f6eb479
Asano-H <princessknk0406@gmail.com>
2017-09-25 13:45:13 +0900

    commit

---
+++ pyramid2.py
@@ -0,0 +1,19 @@
+print("整数を入力してください"end="")
+
+inp=int(input())
+
+i=1
+while i<=inp:
+
+	j=1
+	k=1
+	while j<i-1:
+		print(" "end="")
+		j=j+1
+
+	while k<=(inp-i)*2+1:
+		print("*"end="")
+		k=k+1
+
+	print(" ")
+	i=i+1
