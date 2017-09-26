7390209df5a0012dc197f31570c9d14042530724
Asano-H <princessknk0406@gmail.com>
2017-09-26 11:00:16 +0900

    add

--- max.py
+++ max.py
@@ -1,8 +1,13 @@
 def main():
-	print("4つ数値を入力してください")
+	print("4つの数値を入力してください")
 	data=[input(),input(),input(),input()]
 
 	print("最大値は",max(data),"です")
 
 if __name__=="__main__":
 	main()
+
+"""
+最大値を計算する関数
+max(リスト名)
+"""
