cd09efdb1c22183c11c4a0b427de5c0a6a44cf12
Asano-H <princessknk0406@gmail.com>
2017-09-29 18:50:09 +0900

    commit

--- count.py
+++ count.py
@@ -1,11 +1,14 @@
 import time
-count_limit= int(input())
+count_limit= int(input()) #カウントする秒数を入力する
 
 while count_limit >= 0:
     if count_limit == 0:
-        print("Over")
+        print("Go")
     else:
-        print(count_limit)
-    count_limit -= 1
-    time.sleep(1)
+        print(count_limit) #カウントを画面に出力する
+    count_limit -= 1 #カウントをデクリメント
+    time.sleep(1) #1秒待つ
 
+"""
+1秒ずつカウントダウンする
+"""
