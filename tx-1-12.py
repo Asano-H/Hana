78af74acffa66af5786e34945f2110543973ee73
Asano-H <princessknk0406@gmail.com>
2017-10-07 15:39:46 +0900

    commit

---
+++ tx-1-12.py
@@ -0,0 +1,11 @@
+#読み込んだ三つの整数の和を表示
+print("３つの整数を入力してください")
+print("整数１：")
+a = int(input())
+print("整数２：")
+b = int(input())
+print("整数３：")
+c = int(input())
+
+sum = a+b+c
+print("３つの整数の和は",sum,"です")
