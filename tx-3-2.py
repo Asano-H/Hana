b116d2e6ae5a2d6f3a9c9d8faa97e75383b85ccf
Asano-H <princessknk0406@gmail.com>
2017-10-10 17:11:29 +0900

    fix

--- tx-3-2.py
+++ tx-3-2.py
@@ -1,10 +1,10 @@
 #読み込んだ整数値ＸはＹの約数か
 print("２つの整数を入力してください")
-print("Ｘ")
+print("Ｘ：",end="")
 x = int(input())
-print("Ｙ")
+print("Ｙ：",end="")
 y = int(input())
 if x%y:
     print("ＹはＸの約数ではありません")
-else
+else :
     print("ＹはＸの約数です")
