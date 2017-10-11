62d05eb725453fd2a2c35f90b6786255b4d7c66b
Asano-H <princessknk0406@gmail.com>
2017-10-11 17:11:21 +0900

    commit

---
+++ tx-3-6.py
@@ -0,0 +1,16 @@
+#等価演算子と関係演算子
+print("２つの整数を入力してください")
+print("整数Ａ：",end="")
+a = int(input())
+print("整数Ｂ：",end="")
+b = int(input())
+
+print("等価式の値")
+print("Ａ == Ｂ の値：",a == b)
+print("Ａ != Ｂ の値：",a != b)
+
+print("関係式の値")
+print("Ａ <  Ｂ の値：",a<b)
+print("Ａ <= Ｂ の値：",a<=b)
+print("Ａ >  Ｂ の値：",a<b)
+print("Ａ >= Ｂ の値：",a>=b)
