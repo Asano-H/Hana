0d54bcfb141ebbc5085f8bde55dfe590ac6e06d0
Asano-H <princessknk0406@gmail.com>
2017-10-15 12:20:41 +0900

    committ

--- tx-3-16.py
+++ tx-3-16.py
@@ -2,11 +2,11 @@
 print("何月ですか：",end="")
 month = int(input())
 
-if 3<=month or month<=5:
+if 3<=month and month<=5:
     print("春")
-elif 6<=month or month<=8:
+elif 6<=month and month<=8:
     print("夏")
-elif 9<=month or month<=11:
+elif 9<=month and month<=11:
     print("秋")
-elif month<=2 or month==12:
+elif month==1 or month==2 or month==12:
     print("冬")
