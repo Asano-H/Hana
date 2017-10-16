6b49409c5bc33fe98d98855a2183323827ec5ec7
Asano-H <princessknk0406@gmail.com>
2017-10-16 20:37:59 +0900

    commit

---
+++ tx-4-1.py
@@ -0,0 +1,17 @@
+#読み込んだ値の符号を判定
+while true:
+    print("整数を入力してください：",end="")
+    num = int(input())
+    if no==0:
+        print("その数は０です")
+    elif no>0:
+        print("その数は正です")
+    else
+        print("その数は負です")
+
+    print("もう一度？【Yes・・・０/No・・・９】：")
+    retry = int(input())
+    if retry ==9:
+        break
+    
+
