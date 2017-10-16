bbb1edfaa8c57db69f354aaca83203b86343fb95
Asano-H <princessknk0406@gmail.com>
2017-10-16 20:39:10 +0900

    add

--- tx-4-1.py
+++ tx-4-1.py
@@ -1,12 +1,12 @@
 #読み込んだ値の符号を判定
-while true:
+while True:
     print("整数を入力してください：",end="")
     num = int(input())
     if no==0:
         print("その数は０です")
     elif no>0:
         print("その数は正です")
-    else
+    else :
         print("その数は負です")
 
     print("もう一度？【Yes・・・０/No・・・９】：")
