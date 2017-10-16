71759b0c556fb2f6fc8ff1dc123c5499bbe9aa39
Asano-H <princessknk0406@gmail.com>
2017-10-16 21:07:24 +0900

    commit

---
+++ tx-4-2.py
@@ -0,0 +1,20 @@
+#読み込んだ値の、小さい数から大きい数までの和
+print("２つの整数を入力してください")
+print("整数Ａ：",end="")
+a = int(input())
+print("整数Ｂ：",end="")
+b = int(input())
+if a>b:
+    low = b
+    high = a
+else :
+    low a
+    high = b
+
+num = low
+while True:
+    sum = sum + num
+    num = num + 1
+    if num <= high
+        break
+print(,low,"以上",high,"以下の整数の和は",sum,"です")
