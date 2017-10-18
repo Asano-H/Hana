6040e7999c7f7aec1f46c5cec5b444ca55954c27
Asano-H <princessknk0406@gmail.com>
2017-10-18 19:43:05 +0900

    commit

--- tx-4-4.py
+++ tx-4-4.py
@@ -1,6 +1,8 @@
 #読み込んだ整数値を１までカウントダウン
-print("整数を入力してください")
+print("整数を入力してください：",end="")
+no = int(input())
 if no>=1:
     while no>=1:
-        print(no=no-1,end="")
+        no=no-1
+        print(no)
     print(" ")
